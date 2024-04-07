d4ec5b94228e0cdd2575257b55e19c6bd0a1e3f1
toilavinh <hoangdvinh68@gmail.com>
2024-04-07 15:57:15 +0700

    Service.Basket

---
+++ Services/Service.Basket/Cart.cs
@@ -0,0 +1,19 @@
+﻿namespace Service.Basket;
+
+public class Cart
+{
+    public string UserName { get; set; } = default!;
+
+    public List<CartItem> Items { get; set; } = default!;
+}
+
+public class CartItem
+{
+    public int Quantity { get; set; }
+    
+    public decimal Price { get; set; }
+
+    public string ProductId { get; set; } = default!;
+    
+    public string ImageFile { get; set; } = default!;
+}
--- Services/Service.Basket/Program.cs
+++ Services/Service.Basket/Program.cs
@@ -1,6 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using Service.Basket;
 
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddEndpointsApiExplorer().AddSwaggerGen();
+services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+});
+
+var app = builder.Build();
+app.UseSwagger().UseSwaggerUI();
 app.Map("/", () => "Service.Basket");
+
+app.MapGet("/{userName}", 
+    async (string userName, [FromServices]IDistributedCache redisCache) =>
+    {
+        var cart = await redisCache.GetStringAsync(userName);
+        return cart is null 
+            ? Results.BadRequest("Cart was not existed") 
+            : Results.Ok(JsonConvert.DeserializeObject<Cart>(cart));
+    });
+
+app.MapGet("/list", 
+    async ([FromServices]IDistributedCache redisCache) =>
+    {
+        var keys = await redisCache.GetStringAsync("*");
+    });
+
+app.MapPost("/create", 
+    async (Cart cart, [FromServices]IDistributedCache redisCache) =>
+    {
+        await redisCache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+        return cart;
+    });
+
+app.MapDelete("/delete", 
+    async (string userName, [FromServices]IDistributedCache redisCache) =>
+    {
+        await redisCache.RemoveAsync(userName);
+    });
 
 app.Run();
